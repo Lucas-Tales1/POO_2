d3535ef7fe7429ea5667e352fff563cbe3dd44d2
Lucas-Tales1 <lucastales100@gmail.com>
2024-10-10 16:27:28 -0300

    Lista 2 de Python feita

---
+++ lista02_python/a.py
@@ -0,0 +1,3 @@
+print("Digite seu primeiro nome:")
+nome = input()
+print("Bem-vindo ao Python,",nome)
---
+++ lista02_python/b.py
@@ -0,0 +1,3 @@
+print("Digite seu primeiro completo:")
+nome = input().split()
+print("Bem-vindo ao Python,",nome[0])
